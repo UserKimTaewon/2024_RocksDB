a3569a3ca3d455831b8bdb22448757cf104a04d8
KimTaewon <gimtaewonbeon@gmail.com>
2025-06-02 07:55:03 +0900

    update chronobreakfilter.cpp to log new key format correctly

--- ChronobreakFilter.cpp
+++ ChronobreakFilter.cpp
@@ -5,14 +5,14 @@ ChronobreakFilter::ChronobreakFilter() {}
 
 bool ChronobreakFilter::Filter(int level, const rocksdb::Slice& key, const rocksdb::Slice& value,
                                 std::string* new_value, bool* value_changed) const {
-
-  int64_t timestamp =std::get<1>(deserializeKey(key));
+  LogKey key_data=deserializeKey(key);
+  int64_t timestamp =std::get<1>(key_data);
 
   int64_t now_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
                      std::chrono::system_clock::now().time_since_epoch()).count();
-
+  
   if (now_ms - timestamp > FIFTEEN_SEC_MS) {
-    std::cout << "[FILTER] Dropping key due to TTL_X: " << key.ToString() << std::endl;
+    std::cout << "[FILTER] Dropping key due to TTL_X: " << std::get<2>(key_data) << std::endl;
     return true;
   }
 
